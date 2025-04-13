e1106639257dd45cecae4731f6b95d0ca0e00c9b
LeoMan0 <leoman3628@gmail.com>
2025-04-14 01:38:43 +0200

    JSON and Images added, minor functional listing cards added

--- components.js
+++ components.js
@@ -1,20 +1,55 @@
-const app = Vue.createApp({})
+const app = Vue.createApp({
+
+data() {
+    return {
+      listings: []  // filled via fetch
+    };
+  },
+  created() {
+    fetch('listings.json')
+      .then(res => res.json())
+      .then(data => {
+        this.listings = data;
+      });
+  }
+
+});
+
 
 app.component('site-header', {
   template: `
-    <header>
-      <div class="logo">
-        <img src="Logo.png" alt="House Spy Logo">
-        <span>House Spy</span>
-      </div>
-      <nav>
-        <a href="index.html">Home</a>
-        <a href="#">Buy</a>
-        <a href="#">Sell</a>
-        <a href="#">About</a>
-      </nav>
-    </header>
+	<header>
+  	<div class="logo">
+    	<img src="Logo.png" alt="House Spy Logo">
+    	<span>House Spy</span>
+  	</div>
+  	<nav>
+    	<a href="index.html">Home</a>
+    	<a href="#">Buy</a>
+    	<a href="#">Sell</a>
+    	<a href="#">About</a>
+  	</nav>
+	</header>
   `
-})
+});
+
+app.component('property-list', {
+  props: ['listings'],
+  template: `
+    <section class="listings">
+      <div
+        v-for="listing in listings"
+        :key="listing.SalesID"
+        class="listing-card"
+      >
+        <img
+          :src="'houseImages/' + listing.Image + '.jpg'"
+          :alt="'House ' + listing.SalesID"
+          class="listing-image"
+        >
+      </div>
+    </section>
+  `
+});
 
 app.mount('#app')
